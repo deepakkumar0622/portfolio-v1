ad86b18de0a19b0d31f2c3ec7554af075dfa0932
deepakkumar0622 <deepakramu456@gmail.com>
2025-02-15 17:55:20 +0530

    Certicate/Project changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,13 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
 const listVal = ["Home", "About", "Projects", "Certifications", "Contact"];
+import resume from "/Deepak_Web developer.pdf";
 
 export default function Navbar({ onNavigate }) {
   const [on, seton] = useState(false);
   const showSideBar = () => seton(true);
   const hideSideBar = () => seton(false);
-  const pdfDownload = () => {
-    const link = document.createElement("a");
-    link.href = "./Deepakkumar1.pdf";
-    link.download = "Deepakkumar1.pdf";
-    link.click();
-  };
+
   const handleClick = (e, id) => {
     e.preventDefault(); // Prevent the default anchor behavior
     if (typeof onNavigate === "function") {
@@ -39,23 +35,25 @@ export default function Navbar({ onNavigate }) {
           </ul>
 
           <div className="hidden md:flex">
-            <button className="button " type="button" onClick={pdfDownload}>
-              <span className="button__text">Download cv</span>
-              <span className="button__icon">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 35 35"
-                  id="bdd05811-e15d-428c-bb53-8661459f9307"
-                  data-name="Layer 2"
-                  width={20}
-                  height={20}
-                >
-                  <path d="M17.5,22.131a1.249,1.249,0,0,1-1.25-1.25V2.187a1.25,1.25,0,0,1,2.5,0V20.881A1.25,1.25,0,0,1,17.5,22.131Z"></path>
-                  <path d="M17.5,22.693a3.189,3.189,0,0,1-2.262-.936L8.487,15.006a1.249,1.249,0,0,1,1.767-1.767l6.751,6.751a.7.7,0,0,0,.99,0l6.751-6.751a1.25,1.25,0,0,1,1.768,1.767l-6.752,6.751A3.191,3.191,0,0,1,17.5,22.693Z"></path>
-                  <path d="M31.436,34.063H3.564A3.318,3.318,0,0,1,.25,30.749V22.011a1.25,1.25,0,0,1,2.5,0v8.738a.815.815,0,0,0,.814.814H31.436a.815.815,0,0,0,.814-.814V22.011a1.25,1.25,0,1,1,2.5,0v8.738A3.318,3.318,0,0,1,31.436,34.063Z"></path>
-                </svg>
-              </span>
-            </button>
+            <a href={resume} download="Deepak_Web developer.pdf">
+              <button className="button " type="button">
+                <span className="button__text">Download cv</span>
+                <span className="button__icon">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 35 35"
+                    id="bdd05811-e15d-428c-bb53-8661459f9307"
+                    data-name="Layer 2"
+                    width={20}
+                    height={20}
+                  >
+                    <path d="M17.5,22.131a1.249,1.249,0,0,1-1.25-1.25V2.187a1.25,1.25,0,0,1,2.5,0V20.881A1.25,1.25,0,0,1,17.5,22.131Z"></path>
+                    <path d="M17.5,22.693a3.189,3.189,0,0,1-2.262-.936L8.487,15.006a1.249,1.249,0,0,1,1.767-1.767l6.751,6.751a.7.7,0,0,0,.99,0l6.751-6.751a1.25,1.25,0,0,1,1.768,1.767l-6.752,6.751A3.191,3.191,0,0,1,17.5,22.693Z"></path>
+                    <path d="M31.436,34.063H3.564A3.318,3.318,0,0,1,.25,30.749V22.011a1.25,1.25,0,0,1,2.5,0v8.738a.815.815,0,0,0,.814.814H31.436a.815.815,0,0,0,.814-.814V22.011a1.25,1.25,0,1,1,2.5,0v8.738A3.318,3.318,0,0,1,31.436,34.063Z"></path>
+                  </svg>
+                </span>
+              </button>
+            </a>
           </div>
           <svg
             onClick={showSideBar}
@@ -97,23 +95,25 @@ export default function Navbar({ onNavigate }) {
                 </a>
               </li>
             ))}
-            <button className="button" type="button">
-              <span className="button__text">Download cv</span>
-              <span className="button__icon">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 35 35"
-                  id="bdd05811-e15d-428c-bb53-8661459f9307"
-                  data-name="Layer 2"
-                  width={20}
-                  height={20}
-                >
-                  <path d="M17.5,22.131a1.249,1.249,0,0,1-1.25-1.25V2.187a1.25,1.25,0,0,1,2.5,0V20.881A1.25,1.25,0,0,1,17.5,22.131Z"></path>
-                  <path d="M17.5,22.693a3.189,3.189,0,0,1-2.262-.936L8.487,15.006a1.249,1.249,0,0,1,1.767-1.767l6.751,6.751a.7.7,0,0,0,.99,0l6.751-6.751a1.25,1.25,0,0,1,1.768,1.767l-6.752,6.751A3.191,3.191,0,0,1,17.5,22.693Z"></path>
-                  <path d="M31.436,34.063H3.564A3.318,3.318,0,0,1,.25,30.749V22.011a1.25,1.25,0,0,1,2.5,0v8.738a.815.815,0,0,0,.814.814H31.436a.815.815,0,0,0,.814-.814V22.011a1.25,1.25,0,1,1,2.5,0v8.738A3.318,3.318,0,0,1,31.436,34.063Z"></path>
-                </svg>
-              </span>
-            </button>
+            <a href={resume} download="Deepak_Web developer.pdf">
+              <button className="button" type="button">
+                <span className="button__text">Download cv</span>
+                <span className="button__icon">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 35 35"
+                    id="bdd05811-e15d-428c-bb53-8661459f9307"
+                    data-name="Layer 2"
+                    width={20}
+                    height={20}
+                  >
+                    <path d="M17.5,22.131a1.249,1.249,0,0,1-1.25-1.25V2.187a1.25,1.25,0,0,1,2.5,0V20.881A1.25,1.25,0,0,1,17.5,22.131Z"></path>
+                    <path d="M17.5,22.693a3.189,3.189,0,0,1-2.262-.936L8.487,15.006a1.249,1.249,0,0,1,1.767-1.767l6.751,6.751a.7.7,0,0,0,.99,0l6.751-6.751a1.25,1.25,0,0,1,1.768,1.767l-6.752,6.751A3.191,3.191,0,0,1,17.5,22.693Z"></path>
+                    <path d="M31.436,34.063H3.564A3.318,3.318,0,0,1,.25,30.749V22.011a1.25,1.25,0,0,1,2.5,0v8.738a.815.815,0,0,0,.814.814H31.436a.815.815,0,0,0,.814-.814V22.011a1.25,1.25,0,1,1,2.5,0v8.738A3.318,3.318,0,0,1,31.436,34.063Z"></path>
+                  </svg>
+                </span>
+              </button>
+            </a>
           </ul>
         </div>
         {/* )} */}
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,34 +2,40 @@ import { Fade, Slide } from "react-awesome-reveal";
 
 const cards = [
   {
-    img: "./todocard.jpg",
-    title: "Todo-App",
-    desc: "A simple yet efficient To-Do app designed to help you organize tasks, set priorities, and boost productivity with ease.",
-  },
-  {
-    img: "./e-card.png",
-    title: "E-Commerce website",
-    desc: "An intuitive e-commerce platform offering seamless shopping experiences, secure transactions, and a wide range of products to explore.",
-  },
-  {
     img: "./movie-card.jpeg",
     title: "Movie-App",
     desc: "A dynamic movie app that allows users to browse, search, and discover the latest movies and TV shows with detailed information and trailers.",
+    link: "https://movie-listing-app-cyan.vercel.app/",
   },
   {
-    img: "./collegecard.jpg",
-    title: "College site",
-    desc: "An engaging college blog site for students to share experiences, post articles, and stay updated on campus news and events.",
-  },
-  {
-    img: "./foodcard.jpg",
-    title: "Food-app",
-    desc: "Discover and order your favorite dishes from nearby restaurants with ease. Enjoy fast delivery, exclusive deals, and a world of flavors at your fingertips!",
+    img: "./jio-cinema.png",
+    title: "Jio-cinema clone",
+    desc: "A JioCinema Clone is a streaming platform for movies, TV shows, and live sports with features like authentication, search, and watchlists.",
+    link: "https://deepakkumar0622.github.io/jio-cinema-clone/",
   },
   {
     img: "./portfolio.png",
     title: "Portfolio",
     desc: "Showcasing my expertise in web development, design, and innovation. Explore my projects, skills, and achievements that bring ideas to life.",
+    link: "https://github.com/deepakkumar0622/portfolio-v1",
+  },
+  {
+    img: "./collegecard.jpg",
+    title: "College site",
+    desc: "An engaging college blog site for students to share experiences, post articles, and stay updated on campus news and events.",
+    link: "https://github.com/deepakkumar0622/eduford",
+  },
+  {
+    img: "./todocard.jpg",
+    title: "Todo-App",
+    desc: "A simple yet efficient To-Do app designed to help you organize tasks, set priorities, and boost productivity with ease.",
+    link: "https://github.com/deepakkumar0622/Todo-app",
+  },
+  {
+    img: "./foodcard.jpg",
+    title: "Food-app",
+    desc: "Discover and order your favorite dishes from nearby restaurants with ease. Enjoy fast delivery, exclusive deals, and a world of flavors at your fingertips!",
+    link: "https://github.com/deepakkumar0622/food-app",
   },
 ];
 
@@ -53,50 +59,59 @@ const Project = () => {
           data-aos="zoom-in"
           data-aos-duration="2000"
         >
-          {cards.map((v, i) => (
-            <div
-              key={i}
-              className="relative cursor-pointer text-white group md:w-[400px] w-[300px] h-[200px]  md:mx-0 mx-auto  border rounded-lg overflow-hidden shadow-lg"
-              data-aos="flip-left"
-              data-aos-duration="2000"
-            >
-              <div className="transition-all  duration-500 ">
-                <img
-                  src={v.img}
-                  className="w-full  h-[250px] rounded-lg"
-                  alt={v.title}
-                />
-              </div>
+          {cards.map((v, i) => {
+            // Inline function to handle the button click
+            const handleButtonClick = () => {
+              window.open(v.link, "_blank"); // Opens the link specific to the card in a new tab
+            };
+            return (
+              <div
+                key={i}
+                className="relative cursor-pointer text-white group md:w-[400px] w-[300px] h-[250px] md:mx-0 mx-auto border rounded-lg overflow-hidden shadow-lg"
+                data-aos="flip-left"
+                data-aos-duration="2000"
+              >
+                <div className="transition-all duration-500">
+                  <img
+                    src={v.img}
+                    className="w-full h-[250px] rounded-lg"
+                    alt={v.title}
+                  />
+                </div>
 
-              <div className="absolute left-0 text-white top-[-100%] opacity-0 group-hover:opacity-100 group-hover:top-[0] p-4 w-full h-full bg-black/60 group-hover:backdrop-blur-sm duration-500">
-                <div className="space-y-4">
-                  <Slide cascade>
-                    <h1 className="text-3xl font-bold">{v.title}</h1>
-                    <Fade cascade damping={0.1}>
-                      <div className="text-xs overflow-clip break-words ">
-                        {v.desc}
+                <div className="absolute left-0 text-white top-[-100%] opacity-0 group-hover:opacity-100 group-hover:top-[0] p-4 w-full h-full bg-black/60 group-hover:backdrop-blur-sm duration-500">
+                  <div className="space-y-4">
+                    <Slide cascade>
+                      <h1 className="text-3xl font-bold">{v.title}</h1>
+                      <Fade cascade damping={0.1}>
+                        <div className="text-xs overflow-clip break-words">
+                          {v.desc}
+                        </div>
+                      </Fade>
+                      <div>
+                        <button
+                          className="btn2 max-w-[300px]"
+                          onClick={handleButtonClick}
+                        >
+                          <span className="icon">
+                            <svg
+                              height="24px"
+                              viewBox="0 -960 960 960"
+                              width="24px"
+                              fill="#e8eaed"
+                            >
+                              <path d="M480-80q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-155.5t86-127Q252-817 325-848.5T480-880q83 0 155.5 31.5t127 86q54.5 54.5 86 127T880-480q0 82-31.5 155t-86 127.5q-54.5 54.5-127 86T480-80Zm0-82q26-36 45-75t31-83H404q12 44 31 83t45 75Zm-104-16q-18-33-31.5-68.5T322-320H204q29 50 72.5 87t99.5 55Zm208 0q56-18 99.5-55t72.5-87H638q-9 38-22.5 73.5T584-178ZM170-400h136q-3-20-4.5-39.5T300-480q0-21 1.5-40.5T306-560H170q-5 20-7.5 39.5T160-480q0 21 2.5 40.5T170-400Zm216 0h188q3-20 4.5-39.5T580-480q0-21-1.5-40.5T574-560H386q-3 20-4.5 39.5T380-480q0 21 1.5 40.5T386-400Zm268 0h136q5-20 7.5-39.5T800-480q0-21-2.5-40.5T790-560H654q3 20 4.5 39.5T660-480q0 21-1.5 40.5T654-400Zm-16-240h118q-29-50-72.5-87T584-782q18 33 31.5 68.5T638-640Zm-234 0h152q-12-44-31-83t-45-75q-26 36-45 75t-31 83Zm-200 0h118q9-38 22.5-73.5T376-782q-56 18-99.5 55T204-640Z" />
+                            </svg>
+                          </span>
+                          <span className="text">Link</span>
+                        </button>
                       </div>
-                    </Fade>
-                    <div>
-                      <button className="btn2 max-w-[300px]">
-                        <span className="icon">
-                          <svg
-                            fill="white"
-                            width="20"
-                            viewBox="0 0 16 16"
-                            id="github"
-                          >
-                            <path d="M7.999 0C3.582 0 0 3.596 0 8.032a8.031 8.031 0 0 0 5.472 7.621c.4.074.546-.174.546-.387 0-.191-.007-.696-.011-1.366-2.225.485-2.695-1.077-2.695-1.077-.363-.928-.888-1.175-.888-1.175-.727-.498.054-.488.054-.488.803.057 1.225.828 1.225.828.714 1.227 1.873.873 2.329.667.072-.519.279-.873.508-1.074-1.776-.203-3.644-.892-3.644-3.969 0-.877.312-1.594.824-2.156-.083-.203-.357-1.02.078-2.125 0 0 .672-.216 2.2.823a7.633 7.633 0 0 1 2.003-.27 7.65 7.65 0 0 1 2.003.271c1.527-1.039 2.198-.823 2.198-.823.436 1.106.162 1.922.08 2.125.513.562.822 1.279.822 2.156 0 3.085-1.87 3.764-3.652 3.963.287.248.543.738.543 1.487 0 1.074-.01 1.94-.01 2.203 0 .215.144.465.55.386A8.032 8.032 0 0 0 16 8.032C16 3.596 12.418 0 7.999 0z"></path>
-                          </svg>
-                        </span>
-                        <span className="text">Link</span>
-                      </button>
-                    </div>
-                  </Slide>
+                    </Slide>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
       </div>
     </div>
